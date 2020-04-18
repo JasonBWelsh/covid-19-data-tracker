33e383b4ece98e5bd295960d59294baebef07e33
Jason Welsh <jasonbwelsh@gmail.com>
2020-04-18 11:39:09 -0400

    basic data fetching set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [globalData, setGlobalData] = useState({});
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsloading(true);
+      const response = await axios.get(apiUrl);
+      const countriesResponse = await axios.get(`${apiUrl}/countries`);
+      setGlobalData(response.data);
+      setCountries(countriesResponse.data);
+      setIsloading(false);
+    };
+    fetchData();
+  }, []);
+
+  const apiUrl = 'https://covid19.mathdro.id/api';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <div>Confirmed: {globalData.confirmed.value}</div>
+          <div>Recovered: {globalData.recovered.value}</div>
+          <div>Deaths: {globalData.deaths.value}</div>
+        </>
+      )}
     </div>
   );
 }
